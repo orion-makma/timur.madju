953caed703dadc7883b1891177450795af2920b1
orion-makma <orion.makma@gmail.com>
2024-08-30 20:42:28 +0700

    Add files via upload

---
+++ PT Orion Kp/js/script.js
@@ -0,0 +1,8 @@
+// Toogle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+
